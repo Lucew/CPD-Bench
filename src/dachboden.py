509c19316b9388d2f05ca64ea8f0d7b777f73a86
Dominik Sauerer <dominik.sauerer@gmail.com>
2023-11-03 15:54:59 +0100

    Add new dataset

--- src/dachboden.py
+++ src/dachboden.py
@@ -1,4 +1,9 @@
-def load_dataset(filename):
+import json
+
+import numpy as np
+
+
+def load_dataset(filename, outputname):
     """ Load a CPDBench dataset """
     with open(filename, "r") as fp:
         data = json.load(fp)
@@ -15,6 +20,8 @@ def load_dataset(filename):
     # We normalize to avoid numerical errors.
     mat = (mat - np.nanmean(mat, axis=0)) / np.sqrt(np.nanvar(mat, axis=0, ddof=1))
 
-    np.save("../data/apple.npy", mat)
+    np.save(outputname, mat)
 
     return data, mat
+
+load_dataset("../data/bitcoin.json", "../data/bitcoin.npy")
--- src/main.py
+++ src/main.py
@@ -17,6 +17,14 @@ def get_apple_dataset():
     return CPD2DNdarrayDataset(reshaped_ts, [337])
 
 
+@cpdb.dataset
+def get_bitcoin_dataset():
+    raw_data = np.load("../data/bitcoin.npy")
+    timeseries = raw_data[:, 0]
+    reshaped_ts = np.reshape(timeseries, [1, timeseries.size])
+    return CPD2DNdarrayDataset(reshaped_ts, [569])
+
+
 @cpdb.algorithm
 def execute_esst(signal):
     detector = SST(30)
