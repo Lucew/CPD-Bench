cc7b496ddb657f12297fca371c92d7f5991b63ca
Dominik Sauerer <dominik.sauerer@gmail.com>
2024-01-04 17:51:05 +0100

    Add documentation

--- src/control/CPDDatasetResult.py
+++ src/control/CPDDatasetResult.py
@@ -3,15 +3,26 @@ from enum import Enum
 from exception.ResultSetInconsistentException import ResultSetInconsistentException
 import traceback
 
+from task.Task import Task
+
 
 class ErrorType(str, Enum):
+    """Enum for all error types which can occur during the CPDBench execution"""
     DATASET_ERROR = "DATASET_ERROR"
     ALGORITHM_ERROR = "ALGORITHM_ERROR"
     METRIC_ERROR = "METRIC_ERROR"
 
 
 class CPDDatasetResult:
-    def __init__(self, dataset, algorithms, metrics):
+    """Container for all results of one single dataset including algorithm and metric results"""
+
+    def __init__(self, dataset: Task, algorithms: list[Task], metrics: list[Task]):
+        """Constructs a dataset result with the basic attributes
+        :param dataset: task which created the dataset
+        :param algorithms: list of all algorithm tasks
+        :param metrics: list of all metric tasks
+        """
+
         self._dataset = dataset.get_task_name()
         self._algorithms = [a.get_task_name() for a in algorithms]
         self._metrics = [m.get_task_name() for m in metrics]
@@ -26,24 +37,48 @@ class CPDDatasetResult:
 
         self._errors = []
 
-    def add_algorithm_result(self, indexes, scores, algorithm):
+    def add_algorithm_result(self, indexes: list[int], scores: list[float], algorithm: str) -> None:
+        """Adds an algorithm result with indexes and confidence scores to the result container.
+        :param indexes: list of calculated changepoint indexes
+        :param scores: list of calculated confidence scores respective to the indexes list
+        :param algorithm: name of the calculated algorithm
+        """
+
         if algorithm not in self._algorithms:
             raise ResultSetInconsistentException()
         self._indexes[algorithm] = indexes
         self._scores[algorithm] = scores
 
-    def add_metric_score(self, metric_score, algorithm, metric):
+    def add_metric_score(self, metric_score: float, algorithm: str, metric: str) -> None:
+        """Adds a metric result of an algorithm/dataset to the result container.
+
+        :param metric_score: calculated metric score as float
+        :param algorithm: name of the calculated algorithm
+        :param metric: name of the used metric
+        """
+
         if (algorithm not in self._algorithms
                 or metric not in self._metrics):
             raise ResultSetInconsistentException()
         self._metric_scores[algorithm][metric] = metric_score
 
-    def add_error(self, exception, error_type, algorithm=None, metric=None):
+    def add_error(self, exception: Exception, error_type: ErrorType, algorithm: str = None, metric: str = None) -> None:
+        """Adds a thrown error to the result container.
+        :param exception: the thrown exception object
+        :param error_type: the error type of the thrown exception
+        :param algorithm: name of the algorithm where the exception occurred if applicable
+        :param metric: name of the metric where the exception occurred if applicable
+        """
+
         self._errors.append((type(exception).__name__, ''.join(traceback.format_exception(None, exception,
                                                                                           exception.__traceback__)),
                              error_type, algorithm, metric))
 
-    def get_result_as_dict(self):
+    def get_result_as_dict(self) -> dict:
+        """Returns the result container formatted as dictionary.
+        :returns: the complete results with indexes, scores and metric scores of one dataset as dict
+        """
+
         return {
             self._dataset: {
                 "indexes": self._indexes,
@@ -52,7 +87,11 @@ class CPDDatasetResult:
             }
         }
 
-    def get_errors_as_list(self):
+    def get_errors_as_list(self) -> list:
+        """Returns the list of errors occurred around the dataset.
+        :returns: all errors of the dataset as python list
+        """
+
         return [
             {
                 "dataset": self._dataset,
@@ -64,9 +103,3 @@ class CPDDatasetResult:
             }
             for error in self._errors
         ]
-
-    def get_algorithms(self):
-        return self._algorithms
-
-    def get_metrics(self):
-        return self._metrics
--- src/interface/CPDBench.py
+++ src/interface/CPDBench.py
@@ -1,10 +1,10 @@
 from control.TestbenchController import TestbenchController
 from utils import Logger
-from utils import Utils
 from utils import BenchConfig
 
 
 class CPDBench:
+    """Main class for accessing the CDPBench functions"""
 
     def __init__(self):
         self._datasets = []
@@ -12,8 +12,11 @@ class CPDBench:
         self._metrics = []
         self._logger = None
 
-    def start(self) -> None:
-        BenchConfig.load_config()
+    def start(self, config_file: str = 'config.yml') -> None:
+        """Start the execution of the CDPBench environment
+        :param config_file: Path to the CDPBench configuration file; defaults to 'config.yml'
+        """
+        BenchConfig.load_config(config_file)
         self._logger = Logger.get_application_logger()
         self._logger.debug('CPDBench object created')
         self._logger.info("Starting CPDBench")
@@ -23,17 +26,22 @@ class CPDBench:
         bench.execute_testrun(self._datasets, self._algorithms, self._metrics)
 
     def dataset(self, function):
-        #self._logger.debug(f'Got a dataset function: {Utils.get_name_of_function(function)}')
+        """Decorator for dataset functions which create CPDDataset objects"""
+
+        # self._logger.debug(f'Got a dataset function: {Utils.get_name_of_function(function)}')
         self._datasets.append(function)
         return function
 
     def algorithm(self, function):
-        #self._logger.debug(f'Got an algorithm function: {Utils.get_name_of_function(function)}')
+        """Decorator for algorithm functions which execute changepoint algorithms"""
+
+        # self._logger.debug(f'Got an algorithm function: {Utils.get_name_of_function(function)}')
         self._algorithms.append(function)
         return function
 
     def metric(self, function):
-        #self._logger.debug(f'Got a metric function: {Utils.get_name_of_function(function)}')
+        """Decorator for metric functions which evaluate changepoint results"""
+
+        # self._logger.debug(f'Got a metric function: {Utils.get_name_of_function(function)}')
         self._metrics.append(function)
         return function
-
