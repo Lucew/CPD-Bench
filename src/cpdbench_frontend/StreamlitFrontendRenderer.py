f948e1baa5830756d65fd281b4b4984bc24952ec
Dominik Sauerer <dominik.sauerer@gmail.com>
2024-02-09 10:17:03 +0100

    Add idea for Dash frontend

--- src/cpdbench_frontend/StreamlitFrontendRenderer.py
+++ src/cpdbench_frontend/StreamlitFrontendRenderer.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import subprocess
 import json
+from dash import Dash, html, dcc
+import plotly.express as px
 
 from cpdbench_frontend.FrontendRenderer import FrontendRenderer
 from cpdbench.control import CPDFullResult
@@ -13,12 +15,34 @@ class StreamlitFrontendRenderer(FrontendRenderer):
 
     def show_results(self, results: CPDFullResult) -> None:
         # subprocess.run("ls")
-        subprocess.run(["python", "-m", "streamlit", "run", __file__])
+        app = Dash(__name__)
+
+        # assume you have a "long-form" data frame
+        # see https://plotly.com/python/px-arguments/ for more options
+        df = pd.DataFrame({
+            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Amount": [4, 1, 2, 2, 4, 5],
+            "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+        })
+
+        fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+        fig.write_html("test.html")
+
+        app.layout = html.Div(children=[
+            html.H1(children='Hello Dash'),
+
+            html.Div(children='''
+                Dash: A web application framework for your data.
+            '''),
+
+            dcc.Graph(
+                id='example-graph',
+                figure=fig
+            )
+        ])
+
+        app.run(debug=True)
 
 
 if __name__ == "__main__":
-    df = pd.DataFrame({
-        'first column': [1, 2, 3, 4],
-        'second column': [10, 20, 30, 40]
-    })
-    df
+    StreamlitFrontendRenderer().show_results(0)
